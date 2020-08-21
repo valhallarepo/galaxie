67221c10973af6c9bebee8b9c06afbfdb98882ef
Thiago Alvarenga <thiago.bhzt@gmail.com>
2020-08-21 10:56:11 -0300

    Identação

--- src/app/core/services/ibase.service.ts
+++ src/app/core/services/ibase.service.ts
@@ -1,9 +1,11 @@
 import { Observable } from 'rxjs/internal/Observable';
 
 export interface IBaseService<S, T> {
+
     get(data: S): Observable<T>;
     all(data?: S): Observable<T>;
     save(data: S): Observable<T>;
     patch(id: number | string, data: S): Observable<T>;
     remove(data: number | string): Observable<T>;
+
 }
