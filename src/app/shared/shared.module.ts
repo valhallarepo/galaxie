4109ebdeab1d283a9b911eefa5744344b6c62f65
Thiago de Paula Alvarenga <thiago.alvarenga@oleconsignado.com.br>
2020-11-16 14:21:54 -0300

    Remoção do @angular/flex-layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { Interceptor } from '../internal/application-http-interceptor';
@@ -11,7 +10,6 @@ import { MaterialModule } from './material.module';
     declarations: [],
     imports: [
         Interceptor,
-        FlexLayoutModule,
         FormsModule,
         HttpClientModule,
         MaterialModule,
@@ -26,7 +24,6 @@ import { MaterialModule } from './material.module';
         })
     ],
     exports: [
-        FlexLayoutModule,
         FormsModule,
         HttpClientModule,
         MaterialModule,
@@ -39,7 +36,7 @@ export class SharedModule {
     static forRoot(): ModuleWithProviders<SharedModule> {
         return {
             ngModule: SharedModule
-        }
+        };
     }
 
 }
